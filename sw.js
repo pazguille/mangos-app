e2efe7a0f0df22a403cf3fc4f62937661b2393f6
pazguille <guille87paz@gmail.com>
2026-01-19 19:18:19 -0300

    More vibe coding

--- sw.js
+++ sw.js
@@ -1,8 +1,1 @@
 this.addEventListener('install', async (eve) => { this.skipWaiting(); });
-
-this.addEventListener('fetch', eve => {
-  if (eve.request.headers.has('range')) {
-    return;
-  }
-  eve.respondWith(fetch(eve.request));
-});
